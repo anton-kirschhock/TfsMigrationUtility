bfab702bd806195b1a3f32aba14aa9a8664159c9
Anton Kirschhock <anton.kirschhock@student.ucll.be>
2016-04-06 16:02:12 +0200

    Initial commit: basic project structure

---
+++ TFSMigrationUtility/TfsMigrationUtility.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfsMigrationUtility.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+        }
+    }
+}
