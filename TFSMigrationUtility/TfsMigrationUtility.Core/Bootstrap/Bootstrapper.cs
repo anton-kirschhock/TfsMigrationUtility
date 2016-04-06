721037bbb69e3c27ad094cabe55caf1a88e1c668
Anton Kirschhock <anton.kirschhock@student.ucll.be>
2016-04-06 20:41:54 +0200

    Bootstrapper logic

---
+++ TFSMigrationUtility/TfsMigrationUtility.Core/Bootstrap/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfsMigrationUtility.Core.Bootstrap
+{
+    public static class Bootstrapper
+    {
+        public static void Bootstrap()
+        {
+            //resolve all IBootstrap instances in all
+            var bootstrapInstances = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t =>
+                    t.GetInterfaces().Contains(typeof(IBootstrap)) &&
+                    t.GetConstructor(Type.EmptyTypes) != null)
+                .Select(t => Activator.CreateInstance(t) as IBootstrap);
+
+            foreach (var bootstrapInstance in bootstrapInstances)
+            {
+                bootstrapInstance.Bootstrap();
+            }
+                    
+        }
+    }
+}
